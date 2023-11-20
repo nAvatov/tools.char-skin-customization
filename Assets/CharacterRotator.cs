15b74f975cd0de2b0186244760077dc5b6ba3962
Nariman Avatov <110205226+nAvatov@users.noreply.github.com>
2023-11-20 18:52:31 +0300

    Character rotater fix

--- Assets/CharacterRotator.cs
+++ Assets/CharacterRotator.cs
@@ -5,14 +5,13 @@ using UnityEngine.EventSystems;
 
 public class CharacterRotator : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler {
     [SerializeField] private Transform _charactersTransform;
-    [SerializeField] private float _rotationSpeedMultiplier = 2f;
+    [SerializeField] private float _rotationSpeedMultiplier = 20f;
     private Vector2 _dragStartPoint;
     public void OnBeginDrag(PointerEventData eventData) {
         _dragStartPoint = eventData.position;
     }
 
     public void OnDrag(PointerEventData eventData) {
-        //ApplyRotation(eventData.position.x - _dragStartPoint.x);
         ApplyRotation(eventData.delta.x);
     }
 
@@ -21,6 +20,6 @@ public class CharacterRotator : MonoBehaviour, IDragHandler, IBeginDragHandler, 
     }
 
     private void ApplyRotation(float value) {
-        _charactersTransform.RotateAround(Vector3.up, -value * _rotationSpeedMultiplier * Time.deltaTime);
+        _charactersTransform.Rotate(Vector3.up, -value * _rotationSpeedMultiplier * Time.deltaTime);
     }
 }
